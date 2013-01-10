bc55c046f6b8d6e4198f6901472862ab514408f4
xpostudio4 <ljimenez@stancedata.com>
2013-01-10 19:24:30 -0400

    bootstrap files included, modified profile for user

--- latech/urls.py
+++ latech/urls.py
@@ -14,21 +14,23 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+
     # Uncomment the next line to enable the admin:
      url(r'^$', "latech.views.base"),
-     url(r'^tagit', "latech.views.tagit"),
-
-     url(r'^contact/new/$', 'latech.views.contact_form'), 
+     url(r'^tagit', "latech.views.tagit"), 
      
+
      #Company URLs
      url(r'^company/new/$', CompanyCreate.as_view(), name='company_create'),
      url(r'^company/(\w+)/$', 'latech.views.company_page'),
-#     url(r'^company/(\w+)/edit/$', 'latech.views.company_edit'),
      url('^company/(?P<slug>[\w-]+)/update/$', CompanyUpdate.as_view(), name='company_update'),
 
-
+     #Contact related URLS
      url(r'^profile/(\w+)/$', 'latech.views.user_prof'),
      url(r'^profile/(\w+)/edit/$', 'latech.views.contact_edit'),  
+
+
+     #User sessions and login related URLS
      url(r'^login/$', 'django.contrib.auth.views.login'),
      url(r'^logout/$', 'latech.views.logout_page'),
 
