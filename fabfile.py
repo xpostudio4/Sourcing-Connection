d49fb24e4d8942a5c4802c8c86f82a957525ca61
xpostudio4 <ljimenez@stancedata.com>
2013-01-16 01:21:59 -0400

    fabfile added

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import run, local
+
+def dump_data():
+	local('python manage.py dumpdata companies --indent 4 > companies.json')
+	local('python manage.py dumpdata contacts --indent 4 > contacts.json')
+	local('python manage.py dumpdata auth.user --indent 4 > user.json')
+
+def load_data():
+	local('python manage.py loaddata companies.json')
+	local('python manage.py loaddata contacts.json')
+	local('python manage.py loaddata user.json')
+ 
+    
