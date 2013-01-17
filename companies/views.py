311ec1289a61bb9cec614f6fb6833ea7e894466c
xpostudio4 <ljimenez@stancedata.com>
2013-01-17 18:06:53 -0400

    changed back company view

--- companies/views.py
+++ companies/views.py
@@ -100,7 +100,7 @@ class CompanyView(DetailView):
 class CompanyUpdate(UpdateView):
     model = Company
     form_class = CompanyForm
-    template_name = 'company_page.html'
+    template_name = 'company_form.html'
     success_url = '/company/%(slug)s/'
 
     @method_decorator(login_required)
