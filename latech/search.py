3f76b823f2db1be178b7d332cc963cd62ca898e8
Jearel Alcantara <jalcantara@stancedata.com>
2013-02-07 21:05:59 -0400

    Temporal Search Modification to configure de Database V2

--- latech/search.py
+++ latech/search.py
@@ -84,20 +84,20 @@ def advanced_search(request):
     errors = []
     show_results = False
 
-#    if request.user :
-#        user_id = request.user.id 
+    if request.user :
+        user_id = request.user.id 
 
-#        try:
-#            contact = Contact.objects.get(id = user_id)
-#            if contact.latech_contact == True:
-#                latech= {'contact':True}
-#            else:
-#                latech = {}
-#               
-#        except Contact.DoesNotExist:
-#            latech ={}
-#    else:
-#        latech = {}
+        try:
+            contact = Contact.objects.get(id = user_id)
+            if contact.latech_contact == True:
+                latech= {'contact':True}
+            else:
+                latech = {}
+               
+        except Contact.DoesNotExist:
+            latech ={}
+    else:
+        latech = {}
 
     if 'keywords' in request.GET:
         keywords = request.GET['keywords'].strip()
