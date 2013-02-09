ddc5b4926bc9f5f6ba15ba087d3d6a68d73db20c
Jearel Alcantara <jalcantara@stancedata.com>
2013-02-09 19:25:50 -0400

    Search Implementation

--- latech/search.py
+++ latech/search.py
@@ -162,7 +162,7 @@ def advanced_search(request):
         'show_results': show_results,
         'errors':errors,
         'user_form': user_form,
-#        'hacked_news': hacked_news(),
+        'hacked_news': hacked_news(),
         'latech': latech,
 #        'search_page':search_page()
     })
