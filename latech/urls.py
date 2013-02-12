0e1d6e66d7b266e86f40014815334f2359ec03cf
Jearel Alcantara <jalcantara@stancedata.com>
2013-02-12 19:09:06 -0400

    Modifying the Settings.py

--- latech/urls.py
+++ latech/urls.py
@@ -50,3 +50,7 @@ urlpatterns = patterns('',
  
 )
 
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
