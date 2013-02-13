0d94d686dfebc7fd7380c2acc724ddfe67e44485
Jearel Alcantara <jalcantara@stancedata.com>
2013-02-13 10:04:55 -0400

    Modifying Settings.py and base.html

--- latech/settings.py
+++ latech/settings.py
@@ -11,10 +11,11 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 if os.getenv('HEROKU_ENV') == 'True':
     DATABASES = {'default': {'ENGINE': 'django.db.backends.psycopg2_postgresql',}}
     DATABASES['default'] =  dj_database_url.config()
-    SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
     MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
     MEDIA_URL = '/media/'
 else:
@@ -60,7 +61,7 @@ USE_TZ = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 
 # URL prefix for static files.
@@ -72,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'static'),
+    #os.path.join(os.path.dirname(__file__), 'static'),
 )
 
 # List of finder classes that know how to find static files in
